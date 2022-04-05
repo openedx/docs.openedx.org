8e074c5b124a2a9b0cd61634c58cef074afc2ffc
mark-esme <mark@docemus.io>
2022-04-05 14:31:49 -0400

    remove image width default

--- source/conf.py
+++ source/conf.py
@@ -65,9 +65,7 @@ html_theme = 'sphinx_book_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-images_config = {
-   'default_image_width': '50%'
-}
+
 
 rst_epilog = """
 .. include:: /substitutions.txt
